bc12e52301ad780b35cf7a18d8d0ec20e3c8e501
Greg Rouxel Oldrà <rouxel.g@gmail.com>
2019-01-31 00:29:50 +0100

    FINISH

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  cour = Cour.create!(name: Faker::Book.title)
+end
+
+10.times do
+    student = Student.create!(name: Faker::Name.first_name, cour_id: Faker::Number.between(1, 10))
+  end
